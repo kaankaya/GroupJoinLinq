b782d0d9efe0813dca52c1cf26c544ca4f7cc6f5
Kaan Kaya <kaankaya.0707@gmail.com>
2024-08-26 16:54:16 +0300

    Add project files.

---
+++ GroupJoinLinq/Program.cs
@@ -0,0 +1,49 @@
+﻿using GroupJoinLinq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Listelerimizi oluşturduk verilerimizi ekledik
+        List<Classes> classess = new List<Classes>();
+        classess.Add(new Classes { ClassId = 1, ClassName = "Matematik" });
+        classess.Add(new Classes { ClassId = 2, ClassName = "Türkçe" });
+        classess.Add(new Classes { ClassId = 3, ClassName = "Kimya" });
+        //Listelerimizi oluşturduk verilerimizi ekledik
+        List<Students> students = new List<Students>();
+        students.Add(new Students { StudentId = 1, StudentName = "Ali", ClassId = 1 });
+        students.Add(new Students { StudentId = 2, StudentName = "Ayşe", ClassId = 2 });
+        students.Add(new Students { StudentId = 3, StudentName = "Mehmet", ClassId = 1 });
+        students.Add(new Students { StudentId = 4, StudentName = "Fatma", ClassId = 3 });
+        students.Add(new Students { StudentId = 5, StudentName = "Ahmet", ClassId = 2 });
+
+
+        /*
+         classess tablomuz ile students tablomuzu birleştiriyoruz.
+        GroupJoin yazdıktan sonra ilk birleştireceğimzi tabloyu yazıyoruz
+        daha sonrası eşleştirme kriterlerimizi belirtiriyoruz.Class Id Lere göre eşleştirme yapıyoruz
+        daha sonra anonim bir nesne yapıp çıkarmak istediğimiz verilerimizi seçiyoruz.
+         */
+        var studentsClass = classess.GroupJoin(
+            students,
+            clas => clas.ClassId,
+            student => student.ClassId,
+            (clas, studentGroup) => new
+            {
+                ClassName = clas.ClassName,
+                Students = studentGroup.ToList()
+            });
+
+        foreach (var item in studentsClass)
+        {
+            Console.WriteLine($"Sınıf Adı : {item.ClassName}");
+            foreach (var student in item.Students)
+            {
+                Console.WriteLine($"Bu sınıftaki öğrenciler : {student.StudentName}");
+            }
+            Console.WriteLine(); 
+        }
+
+
+    }
+}
---
+++ GroupJoinLinq/Students.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupJoinLinq
+{
+    public class Students
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int ClassId { get; set; }
+    }
+}
